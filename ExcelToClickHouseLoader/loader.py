40efffbb4cdc8a68ab94577c2fb71b60413d427d
jadesora <>
2024-01-27 15:02:35 +0800

    Fix to separate insert and table creation columns

--- ExcelToClickHouseLoader/loader.py
+++ ExcelToClickHouseLoader/loader.py
@@ -3,8 +3,9 @@ import os
 from clickhouse_driver import Client
 
 class ExcelToClickHouseLoader:
-    def __init__(self, cols, db_name, excel_file_path):
-        self.cols = cols
+    def __init__(self, table_cols, insert_cols, db_name, excel_file_path):
+        self.table_cols = table_cols
+        self.insert_cols = insert_cols
         self.db_name = db_name
         self.excel_file_path = excel_file_path
         self.path = os.getcwd() + "/ExcelDFCH/xlsx/"
@@ -16,10 +17,9 @@ class ExcelToClickHouseLoader:
         self.client.execute('CREATE DATABASE ' + self.db_name)
 
     def create_table(self):
+        columns_definition = ', '.join(f'{col} Int64' if col == 'num' else f'{col} String' for col in self.table_cols)
         table_creation_query = (
-            f'CREATE TABLE {self.db_name}.new_table '
-            '(num Int64, col1 String, col2 String, col3 String, col4 String, '
-            'col5 String, col6 String, col7 String, col8 String, col9 String, sheet String) '
+            f'CREATE TABLE {self.db_name}.new_table ({columns_definition}) '
             'ENGINE MergeTree ORDER BY sheet'
         )
         self.client.execute(table_creation_query)
@@ -36,4 +36,4 @@ class ExcelToClickHouseLoader:
         full_table.reset_index(inplace=True, drop=True)
 
         insert_query = f'INSERT INTO {self.db_name}.new_table VALUES'
-        self.client.insert_dataframe(insert_query, pd.DataFrame(full_table, columns=self.cols))
+        self.client.insert_dataframe(insert_query, pd.DataFrame(full_table, columns=self.insert_cols))
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from ExcelToClickHouseLoader.loader import ExcelToClickHouseLoader
 
 if __name__ == "__main__":
-    db_name = 'team_202401'
-    cols = ['num', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 'sheet']
+    db_name = 'team_202402'
+    table_cols = ['num', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 'sheet']
+    insert_cols = ['num', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 'sheet']
     excel_file_path = 'Book1.xlsx'
 
-    excel_loader = ExcelToClickHouseLoader(cols, db_name, excel_file_path)
+    excel_loader = ExcelToClickHouseLoader(table_cols, insert_cols, db_name, excel_file_path)
     excel_loader.create_database()
     excel_loader.create_table()
     excel_loader.process_sheets()
